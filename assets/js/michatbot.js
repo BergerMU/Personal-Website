f9a99e6a6d63c84220d84d68cd022243701bed20
Jacob Berger <jacobberger1234@gmail.com>
2025-01-28 10:09:02 -0600

    minor updates

--- assets/js/michatbot.js
+++ assets/js/michatbot.js
@@ -104,7 +104,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const generateAPIResponse = async (incomingMessageDiv) => {
         const textElement = incomingMessageDiv.querySelector(".text");
         try {
-            const API_KEY = NEXT_PUBLIC_API_KEY_1;
+            const API_KEY = import.meta.env.VITE_API_KEY;
             const modelType = "gemini-1.5-flash";
             const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/${modelType}:generateContent?key=${API_KEY}`, {
                 method: "POST",
