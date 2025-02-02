dc2a1c43582d28249d05c626eeaf538ec582ae12
Jacob Berger <jacobberger1234@gmail.com>
2025-02-02 14:42:07 -0600

    removed analytics reading

--- michat/michatbot.js
+++ michat/michatbot.js
@@ -1,5 +1,3 @@
-import { Analytics } from "@vercel/analytics/react"
-
 document.addEventListener("DOMContentLoaded", function () {
     const typingForm = document.querySelector(".typing-form");
     const chatList = document.querySelector(".chat-list");
