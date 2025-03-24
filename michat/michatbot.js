5537bcbbeaacff7441d6705ad86be86b04187e34
Jacob Berger <jacobberger1234@gmail.com>
2025-03-24 17:33:50 -0500

    updating gemini version

--- michat/michatbot.js
+++ michat/michatbot.js
@@ -106,7 +106,7 @@ document.addEventListener("DOMContentLoaded", function () {
             const apiKeyData = await apiKeyResponse.json();
             const api_key = apiKeyData.key;
 
-            const modelType = "gemini-1.5-flash";
+            const modelType = "gemma-3-27b-it";
             const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/${modelType}:generateContent?key=${api_key}`, {
                 method: "POST",
                 headers: {
